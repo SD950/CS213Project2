792d1ffe3269e1ed343f176a39a8e68d07a12655
sree2344 <80034904+sree2344@users.noreply.github.com>
2021-10-10 06:07:22 -0400

    Update Profile.java

--- CS213_Project2_StudentRoster/src/proj2/Profile.java
+++ CS213_Project2_StudentRoster/src/proj2/Profile.java
@@ -7,6 +7,47 @@ package proj2;
  * @author Stephen Danso, Sri Vishnu Jayakumar
  *
  */
+import java.text.DecimalFormat;
+
+
 public class Profile {
+    private String name;
+    private Major major; 
+
+
+    public Profile(String name, Major major) {
+        this.name = name; this.major = major;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setMajor(Major major) {
+        this.major = major;
+    }
+    
+    public Major getMajor() {
+        return this.major;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        Profile profile = Profile.class.cast(obj);
+        if (this.name.equals(profile.name) && this.major.equals(profile.major)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return this.name + ":" + this.major;
+    }
 
 }
