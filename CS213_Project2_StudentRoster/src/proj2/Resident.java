d2145818c31524b00fdb04c622ae0821ea0e8578
sree2344 <80034904+sree2344@users.noreply.github.com>
2021-10-10 19:42:12 -0400

    Update Resident.java

--- CS213_Project2_StudentRoster/src/proj2/Resident.java
+++ CS213_Project2_StudentRoster/src/proj2/Resident.java
@@ -7,6 +7,94 @@ package proj2;
  * @author Stephen Danso, Sri Vishnu Jayakumar
  *
  */
-public class Resident {
+import java.text.DecimalFormat;
+
+public class Resident extends Student {
+	
+    private int partTuitionResident = 404*getCreditHour();
+    private static final int fullTimeTuitionResident = 12536;
+    private double aidAmount = 0; private boolean aidRecieved = false;
+
+
+    public Resident(String name, Major major, int creditHourTotal) {
+        super(name, major, creditHourTotal);
+    }
+
+    @Override
+    public void tuitionDue() {
+        if (this.getCreditHour() < creditMinFull) {
+            this.setTuitionDue((partTuitionResident
+            		+ uniFeePart));
+            this.set_calculated(true);
+        } else if ((!(this.getCreditHour() > maxCredNoAddFee))) {
+            this.setTuitionDue((fullTimeTuitionResident 
+            		+ uniFee));
+            this.set_calculated(true);
+        } else {
+            this.setTuitionDue((fullTimeTuitionResident +
+                    ((this.getCreditHour() - maxCredNoAddFee) * 404)
+                    + uniFee));
+            this.set_calculated(true);
+        }
+    }
+    public void setAid(boolean financialAid) {
+        this.aidRecieved = financialAid;
+    }
+
+    public void setAidAmount(double financialAid){
+        this.aidAmount = financialAid;
+    }
+    public boolean financialAid() {
+        return this.aidRecieved;
+    }
+
+    public void setAidAmountResident(double financialAid) {
+        this.setTuitionDue((this.get_DueTuition() - financialAid));
+    }
+
+
+
+
+
+    @Override
+    public String toString() {
+        DecimalFormat df = new DecimalFormat("0.00");
+        df.setGroupingUsed(true);
+        df.setGroupingSize(3);
+        
+        if ((this.getDatePayLast() == null) && (aidRecieved == false)) {
+            return this.getProfile().getName() + ":" + this.getProfile().getMajor() +
+            		":" + this.getCreditHour() + " credit hours:"
+                    + "tuition due:" + df.format(this.get_DueTuition()) + 
+                    ":" + "total payment:" + 
+                    df.format(this.getLastPayment()) +
+                    ":" + "last payment date:" + " --/--/--" + ":" + "resident";
+        } 
+        else if (this.getDatePayLast() != null && (aidRecieved == false)) {
+            return this.getProfile().getName() + ":" + this.getProfile().getMajor() + 
+            		":" + this.getCreditHour() + " credit hours:"
+                    + "tuition due:" + df.format(this.get_DueTuition()) + ":" + "total payment:" + 
+                    df.format(this.getLastPayment()) +
+                    ":" + "last payment date: " + this.getDatePayLast().toString() + ":" + "resident";
+        }
+        else if (this.getDatePayLast() == null && (aidRecieved == true)) {
+            return this.getProfile().getName() + ":" + this.getProfile().getMajor() + 
+            		":" + this.getCreditHour() + " credit hours:"
+                    + "tuition due:" + df.format(this.get_DueTuition()) +
+                    ":" + "total payment:" + 
+                    df.format(this.getLastPayment()) +
+                    ":" + "last payment date:" + " --/--/--" + ":" + "resident" +
+                    ":" + "financial aid $" + df.format(this.aidAmount);
+        } 
+        else if (this.getDatePayLast() != null && (aidRecieved == true)) {
+            return this.getProfile().getName() + ":" + this.getProfile().getMajor() + 
+            		":" + this.getCreditHour() + " credit hours:"
+                    + "tuition due:" + df.format(this.get_DueTuition()) + ":" + "total payment:" + 
+                    df.format(this.getLastPayment()) +
+                    ":" + "last payment date: " + this.getDatePayLast().toString() + ":" + "resident" + 
+                    ":" + "financial aid $" + df.format(this.aidAmount);
+        }
+        return "";
+    }
 
 }
