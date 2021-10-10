68de60dd35328a9081afb2bc35e274b7fbad541f
sree2344 <80034904+sree2344@users.noreply.github.com>
2021-10-10 06:04:42 -0400

    Update NonResident.java

--- CS213_Project2_StudentRoster/src/proj2/NonResident.java
+++ CS213_Project2_StudentRoster/src/proj2/NonResident.java
@@ -7,6 +7,60 @@ package proj2;
  * @author Stephen Danso, Sri Vishnu Jayakumar
  *
  */
-public class NonResident {
+import java.text.DecimalFormat;
 
+
+public class NonResident extends Student {
+    private static final int fullNonResidentTution = 29737;
+    private int partNonResidentTuition = 966 * getCreditHour();
+
+
+    public NonResident(String name, Major major, int creditHourTotal) {
+    	
+        super(name, major, creditHourTotal);
+    }
+
+    @Override
+    public void tuitionDue() {
+        if (this.getCreditHour() < creditMinFull) {
+            this.setTuitionDue((partNonResidentTuition 
+            		+ UNIVERSITY_FEE_PART_TIME));
+            this.set_calculated(true);
+        } else if ((!(this.getCreditHour() > maxCredNoAddFee))) {
+            this.setTuitionDue((fullNonResidentTution
+            		+ uniFee));
+            this.set_calculated(true);
+        } else {
+            this.setTuitionDue((fullNonResidentTution +
+                    ((this.getCreditHour() 
+                    		- maxCredNoAddFee) * 966)
+                    + uniFee));
+            this.set_calculated(true);
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        DecimalFormat df = new DecimalFormat("0.00");
+        df.setGroupingUsed(true);
+        df.setGroupingSize(3);
+        
+        if (this.getDatePayLast() == null) {
+            return this.getProfile().getName() + ":" + this.getProfile().getMajor() + 
+            		":" + this.getCreditHour() + " credit hours:"
+                    + "tuition due:" + df.format(this.get_DueTuition()) + 
+                    ":" + "total payment:" + 
+                    df.format(this.getLastPayment()) +
+            		":" + "last payment date:" + " --/--/--" + ":" + "non-resident";
+        } else {
+            return this.getProfile().getName() + ":" + this.getProfile().getMajor() + 
+            		":" + this.getCreditHour() + " credit hours:"
+                    + "tuition due:" + df.format(this.get_DueTuition()) + 
+                    ":" + "total payment:" + 
+                    df.format(this.getLastPayment()) +
+                    ":" + "last payment date: " + this.getDatePayLast().toString() + 
+                    ":" + "non-resident";
+        }
+    }
 }
