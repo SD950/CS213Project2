bc0a91e6c9ec8da5f235777c76d38c26618eb80d
SD950 <Stephendanso14@gmail.com>
2021-10-11 19:31:50 -0400

    Updated RunProject2.java(with comments)

--- CS213_Project2_StudentRoster/src/proj2/RunProject2.java
+++ CS213_Project2_StudentRoster/src/proj2/RunProject2.java
@@ -4,11 +4,12 @@
 package proj2;
 
 /**
+ * Main class used to run the program
+ * 
  * @author Stephen Danso, Sri Vishnu Jayakumar
- *
  */
 public class RunProject2 {
-    public static void main(String[] args) {
-        new TuitionManager().run();
-    }
+	public static void main(String[] args) {
+		new TuitionManager().run();
+	}
 }
