83cd5f363ac8e7584d9fc32f3085459382eaf2e6
SD950 <Stephendanso14@gmail.com>
2021-10-11 19:27:39 -0400

    Updated Major.java(with comments)

--- CS213_Project2_StudentRoster/src/proj2/Major.java
+++ CS213_Project2_StudentRoster/src/proj2/Major.java
@@ -1,5 +1,10 @@
 package proj2;
 
+/**
+ * Enum class that holds a list of the available majors
+ * 
+ * @author Stephen Danso, Sri Vishnu Jayakumar
+ */
 public enum Major {
 	BA, CS, EE, IT, ME
 }
