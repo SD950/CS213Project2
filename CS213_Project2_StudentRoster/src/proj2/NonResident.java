cb6d3ae6eab4abcaf3ce39399e446cdd78747d60
sree2344 <80034904+sree2344@users.noreply.github.com>
2021-10-11 02:53:15 -0400

    Update NonResident.java

--- CS213_Project2_StudentRoster/src/proj2/NonResident.java
+++ CS213_Project2_StudentRoster/src/proj2/NonResident.java
@@ -9,58 +9,44 @@ package proj2;
  */
 import java.text.DecimalFormat;
 
-
 public class NonResident extends Student {
-    private static final int fullNonResidentTution = 29737;
-    private int partNonResidentTuition = 966 * getCreditHour();
+	private int partNonResidentTuition = 966 * getCreditHour();
 
+	public NonResident(String name, Major major, int creditHourTotal) {
 
-    public NonResident(String name, Major major, int creditHourTotal) {
-    	
-        super(name, major, creditHourTotal);
-    }
+		super(name, major, creditHourTotal);
+	}
 
-    @Override
-    public void tuitionDue() {
-        if (this.getCreditHour() < creditMinFull) {
-            this.setTuitionDue((partNonResidentTuition 
-            		+ UNIVERSITY_FEE_PART_TIME));
-            this.set_calculated(true);
-        } else if ((!(this.getCreditHour() > maxCredNoAddFee))) {
-            this.setTuitionDue((fullNonResidentTution
-            		+ uniFee));
-            this.set_calculated(true);
-        } else {
-            this.setTuitionDue((fullNonResidentTution +
-                    ((this.getCreditHour() 
-                    		- maxCredNoAddFee) * 966)
-                    + uniFee));
-            this.set_calculated(true);
-        }
-    }
+	@Override
+	public void tuitionDue() {
+		if (this.getCreditHour() < creditMinFull) {
+			this.setTuitionDue((partNonResidentTuition + uniFeePart));
+			this.set_calculated(true);
+		} else if ((!(this.getCreditHour() > maxCredNoAddFee))) {
+			this.setTuitionDue((29737 + uniFee));
+			this.set_calculated(true);
+		} else {
+			this.setTuitionDue((29737 + ((this.getCreditHour() - maxCredNoAddFee) * 966) + uniFee));
+			this.set_calculated(true);
+		}
+	}
 
+	@Override
+	public String toString() {
+		DecimalFormat df = new DecimalFormat("0.00");
+		df.setGroupingUsed(true);
+		df.setGroupingSize(3);
 
-    @Override
-    public String toString() {
-        DecimalFormat df = new DecimalFormat("0.00");
-        df.setGroupingUsed(true);
-        df.setGroupingSize(3);
-        
-        if (this.getDatePayLast() == null) {
-            return this.getProfile().getName() + ":" + this.getProfile().getMajor() + 
-            		":" + this.getCreditHour() + " credit hours:"
-                    + "tuition due:" + df.format(this.get_DueTuition()) + 
-                    ":" + "total payment:" + 
-                    df.format(this.getLastPayment()) +
-            		":" + "last payment date:" + " --/--/--" + ":" + "non-resident";
-        } else {
-            return this.getProfile().getName() + ":" + this.getProfile().getMajor() + 
-            		":" + this.getCreditHour() + " credit hours:"
-                    + "tuition due:" + df.format(this.get_DueTuition()) + 
-                    ":" + "total payment:" + 
-                    df.format(this.getLastPayment()) +
-                    ":" + "last payment date: " + this.getDatePayLast().toString() + 
-                    ":" + "non-resident";
-        }
-    }
+		if (this.getDatePayLast() == null) {
+			return this.getProfile().getName() + ":" + this.getProfile().getMajor() + ":" + this.getCreditHour()
+					+ " credit hours:" + "tuition due:" + df.format(this.get_DueTuition()) + ":" + "total payment:"
+					+ df.format(this.getLastPayment()) + ":" + "last payment date:" + " --/--/--" + ":"
+					+ "non-resident";
+		} else {
+			return this.getProfile().getName() + ":" + this.getProfile().getMajor() + ":" + this.getCreditHour()
+					+ " credit hours:" + "tuition due:" + df.format(this.get_DueTuition()) + ":" + "total payment:"
+					+ df.format(this.getLastPayment()) + ":" + "last payment date: " + this.getDatePayLast().toString()
+					+ ":" + "non-resident";
+		}
+	}
 }
