ee3d87369a92140920c2e21131bf60c68237ce85
SD950 <Stephendanso14@gmail.com>
2021-10-11 19:29:09 -0400

    Updated Profile.java(with comments)

--- CS213_Project2_StudentRoster/src/proj2/Profile.java
+++ CS213_Project2_StudentRoster/src/proj2/Profile.java
@@ -1,53 +1,90 @@
-/**
- * 
- */
 package proj2;
 
-/**
- * @author Stephen Danso, Sri Vishnu Jayakumar
- *
- */
 import java.text.DecimalFormat;
 
-
+/**
+ * Class that defines the profile of a student which includes name and major
+ * 
+ * @author Stephen Danso, Sri Vishnu Jayakumar
+ */
 public class Profile {
-    private String name;
-    private Major major; 
+	private String name;
+	private Major major;
 
+	/**
+	 * Constructor used to create Profile object using the name and major as
+	 * parameters
+	 * 
+	 * @param name  student's name
+	 * @param major student's major
+	 */
+	public Profile(String name, Major major) {
+		this.name = name;
+		this.major = major;
+	}
 
-    public Profile(String name, Major major) {
-        this.name = name; this.major = major;
-    }
+	/**
+	 * Helper method to get the name of student
+	 * 
+	 * @return String name
+	 */
+	public String getName() {
+		return this.name;
+	}
 
-    public String getName() {
-        return this.name;
-    }
+	/**
+	 * Helper method to set the name
+	 * 
+	 * @param name student name string
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
-    public void setMajor(Major major) {
-        this.major = major;
-    }
-    
-    public Major getMajor() {
-        return this.major;
-    }
+	/**
+	 * Helper method to set the major
+	 * 
+	 * @param major student major Major object
+	 */
+	public void setMajor(Major major) {
+		this.major = major;
+	}
 
+	/**
+	 * Helper method used to get student's major
+	 * 
+	 * @return Major enum
+	 */
+	public Major getMajor() {
+		return this.major;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        Profile profile = Profile.class.cast(obj);
-        if (this.name.equals(profile.name) && this.major.equals(profile.major)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+	/**
+	 * Overriden equals() method to check the equality of two profiles using
+	 * students name and major
+	 * 
+	 * @param obj object to be compared in equals method
+	 * @return boolean true or false depending on whether the profiles are the same
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		Profile profile = Profile.class.cast(obj);
+		if (this.name.equals(profile.name) && this.major.equals(profile.major)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-    @Override
-    public String toString() {
-        return this.name + ":" + this.major;
-    }
+	/**
+	 * Overriden toString() method that returns the string representation of name
+	 * and major
+	 * 
+	 * @return String name and major
+	 */
+	@Override
+	public String toString() {
+		return this.name + ":" + this.major;
+	}
 
 }
